23ccb47d61cad6cf2c1ab86f79be86b042c29a2f
somanayr <somanayr@gmail.com>
2013-11-08 22:58:40 -0500

    Model arguments

--- model.py
+++ model.py
@@ -4,11 +4,18 @@ Created on Oct 28, 2013
 import numpy
 
 class Model:
-    def __init__(self, loops):
+    def __init__(self, models=None, loop=None):
         '''
         Generates a model from list of similar loops, loops
         '''
-        self.loops = loops
+        if models is None and loop is None:
+            raise Exception("Must provide models to merge or loop")
+        self.loop = loop
+        self.models = models
+        self.sses = models[0].sses if models is not None else sorted([(loop.l_type, loop.l_anchor), (loop.r_type, loop.l_anchor)])
+        self.phi
+        self.theta
+        self.positions
         #todo
         
     def score(self, loop):
