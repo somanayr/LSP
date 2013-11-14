69a080a47d406b3009a3b51c3cc1dfefa8dbec43
somanayr <somanayr@gmail.com>
2013-11-13 19:25:40 -0500

    Model commenting and cleaning up

--- model.py
+++ model.py
@@ -24,13 +24,13 @@ class Model:
         self.ssesSignature = ssesSignature
         self.seq = seq
         self.size = size
-        self.__loops = None #lazy loaded, NOT SAFE
+        self.__loops = None #lazy loaded, not safe to call, use get_loops instead
         
     @classmethod
     def fromLoop(cls, loop):
         """Returns a Model representing the loop"""
         sses = sorted([(loop.l_type, loop.l_anchor), (loop.r_type, loop.r_anchor)])
-        return Model([loop], loop.atoms, [sses[0][1], sses[1][1]], [sses[0][0], sses[1][0]], Model.gen_seq([loop.seq]), 1);
+        return Model([loop], loop.atoms, [sses[0][1], sses[1][1]], [sses[0][0], sses[1][0]], Model.__gen_seq([loop.seq]), 1)
         
     @classmethod
     def fromModels(cls, m1, m2):
@@ -42,11 +42,11 @@ class Model:
 
         #find necessary vectors
         sOffsetV = [m1.sses[1][0].__dict__[c] - m1.sses[0][0].__dict__[c] for c in 'xyz']
-        sSSEV = Model.get_sse_vector(m1.sses[0], m1.positions[0])
+        sSSEV = Model.__get_sse_vector(m1.sses[0], m1.positions[0])
         
         oOffsetV = [m2.sses[1][0].__dict__[c] - m2.sses[0][0].__dict__[c] for c in 'xyz']
-        oSSE0V = Model.get_sse_vector(m1.sses[0], m1.positions[0])
-        oSSE1V = Model.get_sse_vector(m1.sses[1], m1.positions[-1])
+        oSSE0V = Model.__get_sse_vector(m1.sses[0], m1.positions[0])
+        oSSE1V = Model.__get_sse_vector(m1.sses[1], m1.positions[-1])
         
         oSSEV = oSSE0V
         
@@ -59,57 +59,41 @@ class Model:
             scoreNormal = m1.__compute_scores(m2.sses[0], m2.sses[1], m2.positions)
             revPos = [k for k in reversed(m2.positions)]
             scoreReversed = m1.__compute_scores(m2.sses[1], m2.sses[0], revPos)
+            
+            #if we get a better score with the reversed orientation, flip the orientation
             if(scoreReversed < scoreNormal):
                 otherPositions = revPos
                 oSSEV = oSSE1V
                 oOffsetV = negative(oOffsetV)
                 oOrigin = m2.sses[1][0]
-            revPos = None
+                
+            revPos = None #clear the memory
         
         sFrame = TransformFrame.createFromVectors(sOrigin, transform.Vec.from_array(sOffsetV), transform.Vec.from_array(sSSEV))
         oFrame = TransformFrame.createFromVectors(oOrigin, transform.Vec.from_array(oOffsetV), transform.Vec.from_array(oSSEV))
         
+        #Determine the position
         positions = []
         for i in range(len(m1.positions)):
+            #transform
             sPoint = sFrame.transformInto(m1.positions[i]) #we transform from global space to loop space so that we have a relative points... xyz from the SSE, not the origin
             oPoint = oFrame.transformInto(otherPositions[i])
-#             print("Transformed %s to %s and %s to %s" % (str(m1.positions[i] if type(m1.positions[i]) is Vec else Vec(m1.positions[i])), str(sPoint), str(m2.positions[i] if type(m2.positions[i]) is Vec else Vec(m2.positions[i])), str(oPoint)))
+            
+            #Position is m1.positions[i] - sPoint + (sPoint + oPoint) / 2, in other words, the average, displaced back to the position of m1
             v = Vec({'xyz'[j]: m1.positions[i].__dict__['xyz'[j]] - sPoint[j] + (sPoint[j] + oPoint[j]) / 2 for j in range(3)})
-#             print "Merging %s and %s to %s" % (str(m1.positions[i] if type(m1.positions[i]) is Vec else Vec(m1.positions[i])), str(m2.positions[i] if type(m2.positions[i]) is Vec else Vec(m2.positions[i])), str(v))
+            
             positions.append(v)
         
-        return Model([m1,m2], positions, m1.sses, m1.ssesSignature, m1.merge_seqs(m2, m1.size, m2.size), m1.size + m2.size)
+        return Model([m1,m2], positions, m1.sses, m1.ssesSignature, m1.__merge_seqs(m2, m1.size, m2.size), m1.size + m2.size)
         
     @classmethod
-    def get_sse_vector(cls, sse_atoms, loop_anchor):
+    def __get_sse_vector(cls, sse_atoms, loop_anchor):
+        """Get the vector along an SSE. If the SSE contains only 1 atom, it will return the vector from that atom to the loop anchor,
+        which is the first element in the loop. Otherwise, it will return the vector between the atom at 25% and 75%"""
         if(len(sse_atoms) == 1):
             return [loop_anchor.__dict__[c] - sse_atoms[0].__dict__[c] for c in 'xyz']
         else:
             return [sse_atoms[int((len(sse_atoms) * .25))].__dict__[c] - sse_atoms[int((len(sse_atoms).__dict__[c] * .75))] for c in 'xyz']
-        
-    def score(self, loop):
-        """Scores how well the loop matches the Model"""
-        
-        sys.stderr.write("Score is deprecated, please do not use!\n")
-        
-        if(len(self.loops) == 0):
-            return float("inf")
-        
-        total = 0
-        for l in self.loops:
-            total += l.closeness(loop)
-        return total / len(self.loops)
-    
-    def compareOLD(self, other):
-        """Scores how close two Models are."""
-        #todo
-        if (len(other.loops) == 0):
-            return float("inf")
-        
-        total = 0
-        for loop in other.loops:
-            total += self.score(loop)
-        return total / len(other.loops)
     
     def compare(self, other):
         """
@@ -124,9 +108,9 @@ class Model:
             return float('inf')
         
         if self.ssesSignature[0] == self.ssesSignature[1]: #if ends are helix/helix or sheet/sheet, then you don't know which end aligns with which
+            #Try both, return the best
             return max(self.__compute_scores(other.sses[0], other.sses[1], other.positions),
-                       self.__compute_scores(other.sses[1], other.sses[0], [k for k in reversed(other.positions)])
-                       )
+                       self.__compute_scores(other.sses[1], other.sses[0], [k for k in reversed(other.positions)]))
         else:
             return self.__compute_scores(other.sses[0], other.sses[1], other.positions)
         
@@ -135,27 +119,19 @@ class Model:
     def __compute_scores(self, other_sses_0, other_sses_1, other_positions):
         """Private method do not call unless you know what you're doing! Computes how well our model matches up against the given data"""
         #get necessary vectors
-#         print(self.sses)
         sOffsetV = [self.sses[1][0].__dict__[c] - self.sses[0][0].__dict__[c] for c in 'xyz']
-#         sOffsetV = [self.positions[-1].__dict__[c] - self.positions[0].__dict__[c] for c in 'xyz']
-#         sOffsetV = 
-#         print [self.positions[-1].__dict__[c] for c in 'xyz'], [self.positions[0].__dict__[c] for c in 'xyz']
-#         sSSE0V = [self.positions[0].__dict__[c] - mean([atom.__dict__[c] for atom in self.sses[0]]) for c in 'xyz']
-#         sSSE1V = [self.positions[-1].__dict__[c] - mean([atom.__dict__[c] for atom in self.sses[1]]) for c in 'xyz']
-        sSSE0V = Model.get_sse_vector(self.sses[0], self.positions[0])
-        sSSE1V = Model.get_sse_vector(self.sses[1], self.positions[-1]) 
+        sSSE0V = Model.__get_sse_vector(self.sses[0], self.positions[0])
+        sSSE1V = Model.__get_sse_vector(self.sses[1], self.positions[-1]) 
 
         
         oOffsetV = [other_sses_1[0].__dict__[c] - other_sses_0[0].__dict__[c] for c in 'xyz']
-#         oOffsetV = [other_positions[-1].__dict__[c] - other_positions[0].__dict__[c] for c in 'xyz']
-#         oSSE0V = [other_positions[0].__dict__[c] - mean([atom.__dict__[c] for atom in other_sses_0]) for c in 'xyz']
-#         oSSE1V = [other_positions[-1].__dict__[c] - mean([atom.__dict__[c] for atom in other_sses_1]) for c in 'xyz']
-        oSSE0V = Model.get_sse_vector(other_sses_0, other_positions[0])
-        oSSE1V = Model.get_sse_vector(other_sses_1, other_positions[-1])
+        oSSE0V = Model.__get_sse_vector(other_sses_0, other_positions[0])
+        oSSE1V = Model.__get_sse_vector(other_sses_1, other_positions[-1])
         
         sFrame = TransformFrame.createFromVectors(self.sses[0][0], transform.Vec.from_array(sOffsetV), transform.Vec.from_array(sSSE0V))
         oFrame = TransformFrame.createFromVectors(other_sses_0[0], transform.Vec.from_array(oOffsetV), transform.Vec.from_array(oSSE0V))
         
+        #Average RMSD
         total = 0
         for i in range(len(self.positions)):
             sPoint = sFrame.transformInto(self.positions[i]) #we transform from global space to loop space so that we have a relative points... xyz from the SSE, not the origin
@@ -166,6 +142,7 @@ class Model:
         if(total > 2): #must be at most 2 angstroms apart
             return float('inf')
         
+        #Theta and phi are the angles between the SSE and anchor-anchor vector
         s_theta = arccos(dot(sSSE0V, negative(sOffsetV)) / (norm(sSSE0V) * norm(sOffsetV)))
         s_phi = arccos(dot(sSSE1V, sOffsetV) / (norm(sSSE1V) * norm(sOffsetV)))
         
@@ -193,7 +170,7 @@ class Model:
         loops += self.__loops        
    
     @classmethod
-    def gen_seq(cls, loops):
+    def __gen_seq(cls, loops):
         """helper method to generate a probabilistic sequence representing a cluster of loops (loops must be the same length)"""
 
         num_loops = len(loops)
@@ -216,17 +193,17 @@ class Model:
                     seq[i][aa] = 1.0
         
         #normalize the sequence (so probabilities sum to 1), then return
-        return Model.normalize_seq(seq, num_loops)
+        return Model.__normalize_seq(seq, num_loops)
     
     @classmethod
-    def normalize_seq(cls, seq, n):
+    def __normalize_seq(cls, seq, n):
         """helper method to normalize a probabilistic sequence, given the total number of entries per sequence location"""
         for pos in seq:
             for aa in pos.keys():
                 pos[aa] /= n
         return seq
     
-    def merge_seqs(self, other, n1, n2):
+    def __merge_seqs(self, other, n1, n2):
         """helper method to merge two probabilistic sequences, given two models (each with their own representative sequence) and their respective weights (i.e. number of loops represented)"""
         merged_seq = [{} for i in range(len(self.seq))]
         weight1 = (n1/float(n1 + n2))
@@ -251,11 +228,6 @@ class Model:
         return merged_seq
         
     def __str__(self):
-#         loops = []
-#         self.get_loops(loops)
-#         mean_displacement = self.seq
-#         loops = []
-#         self.get_loops(loops)
         
         return ("Model[\n\tsize=%d,\n\tsignature=%s,\n\tsses=[\n\t\t%s,\n\t\t%s\n\t]\n\tloop_len=%d,\n\tseq=%s,\n\tpositions=%s\n]" % (
                                                                                         self.size,
